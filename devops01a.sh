488266b903309e071d118f819765e89df11838f0
Gbmonmon <TaiwanChenYu@gmail.com>
2018-09-24 17:52:56 -0700

    create devops01a.sh.

---
+++ devops01a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "1. number of volumes, size of each volume, free space on each volume"
+sudo df -h
+echo " "
+echo "2. number cpus/cores, information about the cpus/core"
+system_profiler SPHardwareDataType | grep "  Memory:"
+system_profiler SPHardwareDataType | grep Cores:
+system_profiler SPHardwareDataType | grep Processors:
+echo " "
+echo "3. your mac address and ip address"
+ip=$(ipconfig getifaddr en0)
+echo "IP: ${ip}"
+address=$(ifconfig en1 | awk '/ether/{print $2}')
+echo "Address: ${address}"
