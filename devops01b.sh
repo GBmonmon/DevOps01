ab56ea7117ec674336455f3fc8ba39c65c8e460e
Gbmonmon <TaiwanChenYu@gmail.com>
2018-10-07 15:06:30 -0700

    Allowing user to pass several arguments.

--- devops01b.sh
+++ devops01b.sh
@@ -1,47 +1,53 @@
 #!/bin/bash
 
+numberCmd=$#
+options_input=($@)
+echo "Usage > ./devops01b.sh [OPTIONs]"
+echo "EXAMPLE > ./devops01b.sh -RAM -CPU"
+echo " " 
 
-# $# --> number of parameter passing in.
-if [ $# != 1 ]; then
-  echo "Only takes in on parameter..."
-  echo "Usage 1: ./devops01b.sh volumes
-      2: ./devops01b.sh cpu
-      3: ./devops01b.sh ram
-      4: ./devops01b.sh network
-      5: ./devops01b.sh all"  
-else 
-  option=$1
-  optionLower=$(echo $option | tr '[:upper:]' '[:lower:]')
-  if [ "$optionLower" == "volumes"  ]; then
-    sudo df -h  
-  elif [ "$optionLower"  == "cpu" ]; then
-    system_profiler SPHardwareDataType | grep Cores:
-    system_profiler SPHardwareDataType | grep Processors:
-  elif [ "$optionLower" == "ram" ]; then
-    system_profiler SPHardwareDataType | grep "  Memory:"
-  elif [ "$optionLower" == "network" ]; then
+
+
+for option in ${options_input[*]}; do
+  option=$(echo $option | tr '[:lower:]' '[:upper:]')
+  case $option in 
+  -RAM) 
+    echo "----------ram----------"
+    system_profiler SPHardwareDataType | grep "  Memory:";;
+
+  -CPU) 
+    echo "----------cpu----------"
+    system_profiler SPHardwareDataType | grep Cores: 
+    system_profiler SPHardwareDataType | grep Processors:;;
+
+  -NETWORK) 
+    echo "----------network----------"
     ip=$(ipconfig getifaddr en0)
     echo "IP: ${ip}"
     address=$(ifconfig en1 | awk '/ether/{print $2}')
-    echo "Address: ${address}"
-  elif [ "$optionLower" == "all" ]; then
-    echo "-----volumes-----"
+    echo "Address: ${address}";;
+  
+  -VOLUMES)
+    echo "----------volumes----------"
+    sudo df -h;;
+
+  -ALL) 
+    echo "----------all-----------"
+    echo "+++++volumes+++++"
     sudo df -h
-    echo "-----cpu-----"
+    echo "+++++cpu+++++"
+    echo " "
     system_profiler SPHardwareDataType | grep Cores:
     system_profiler SPHardwareDataType | grep Processors:
-    echo "-----ram-----"
+    echo " "
+    echo "+++++ram++++++"
     system_profiler SPHardwareDataType | grep "  Memory:"
-    echo "-----network-----"
+    echo " "
+    echo "+++++network+++++"
     ip=$(ipconfig getifaddr en0)
     echo "IP: ${ip}"
     address=$(ifconfig en1 | awk '/ether/{print $2}')
-    echo "Address: ${address}" 
-  else
-    echo "Usage 1: ./devops01b.sh volumes
-        2: ./devops01b.sh cpu
-        3: ./devops01b.sh ram
-        4: ./devops01b.sh network
-        5: ./devops01b.sh all"  
-  fi
-fi
+    echo "Address: ${address}";;
+  esac
+done
+
